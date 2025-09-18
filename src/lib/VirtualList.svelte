728600c8fad36362a849ddac22d018dbebf5de28
Charles Kaup <cv.kaup@gmail.com>
2025-09-18 18:10:13 -0500

    Convert to Svelte 5, begin refactor
    Getting lots of performance issues from this component in presage - mainly coming from forced layout reflows caused by the `refresh` function

--- src/lib/VirtualList.svelte
+++ src/lib/VirtualList.svelte
@@ -1,143 +1,156 @@
-<script lang="ts" generics="T">
-	import { onMount, tick } from 'svelte';
+<script
+	lang="ts"
+	generics="T"
+>
+	import { onMount, tick, type Snippet } from 'svelte'
 
-	// props
-	export let items: Array<T>;
-	export let height: CSSStyleValue = '100%';
-	export let itemHeight: undefined | number = undefined;
-
-	// read-only, but visible to consumers via bind:start
-	export let start = 0;
-	export let end = 0;
-	// local state
-	let height_map: Array<number> = [];
-	let rows: HTMLCollectionOf<HTMLElement>;
-	let viewport: Element;
-	let contents: Element;
-	let viewport_height = 0;
-	let visible: Array<{ index: number; data: T }>;
-	let mounted: boolean;
-
-	let top = 0;
-	let bottom = 0;
-	let average_height: number;
-
-	$: visible = items.slice(start, end).map((data, i) => {
-		return { index: i + start, data };
-	});
-
-	// whenever `items` changes, invalidate the current heightmap
-	$: if (mounted) refresh(items, viewport_height, itemHeight);
-
-	async function refresh(items: Array<T>, viewport_height: number, itemHeight: number | undefined) {
-		const isStartOverflow = items.length < start;
-
-		if (isStartOverflow) {
-			await scrollToIndex(items.length - 1, { behavior: 'auto' });
-		}
-
-		const { scrollTop } = viewport;
-
-		await tick(); // wait until the DOM is up to date
-
-		let content_height = top - scrollTop;
-		let i = start;
-
-		while (content_height < viewport_height && i < items.length) {
-			let row = rows[i - start];
-
-			if (!row) {
-				end = i + 1;
-				await tick(); // render the newly visible row
-				row = rows[i - start];
-			}
-
-			const row_height = (height_map[i] = itemHeight || row.offsetHeight);
-			content_height += row_height;
-			i += 1;
-		}
-
-		end = i;
-
-		const remaining = items.length - end;
-		average_height = (top + content_height) / end;
-
-		bottom = remaining * average_height;
-		height_map.length = items.length;
+	interface Props {
+		items: Array<T>
+		height?: CSSStyleValue
+		itemHeight?: undefined | number
+		start?: number
+		end?: number
+		children: Snippet<[{ item: T }]>
 	}
 
-	async function handle_scroll() {
-		const { scrollTop } = viewport;
+	let {
+		items,
+		height = '100%',
+		itemHeight = undefined,
+		start = $bindable(0),
+		end = $bindable(0),
+		children,
+	}: Props = $props()
 
-		const old_start = start;
+	let heightMap: Array<number> = []
+	let rows: HTMLCollectionOf<HTMLElement>
+	let viewport: Element | undefined = $state.raw()
+	let contents: Element | undefined = $state.raw()
+	let viewportHeight = $state(0)
+	let visible: Array<{ index: number; data: T }> = $derived(
+		items.slice(start, end).map((data, i) => {
+			return { index: i + start, data }
+		}),
+	)
+	let mounted: boolean = $state(false)
 
-		for (let v = 0; v < rows.length; v += 1) {
-			height_map[start + v] = itemHeight || rows[v].offsetHeight;
+	let top = $state(0)
+	let bottom = $state(0)
+	let averageHeight: number
+
+	$inspect('heightMap', heightMap)
+
+	async function refresh(items: Array<T>, viewportHeight: number, itemHeight: number | undefined) {
+		const isStartOverflow = items.length < start
+
+		if (isStartOverflow) {
+			await scrollToIndex(items.length - 1, { behavior: 'auto' })
 		}
 
-		let i = 0;
-		let y = 0;
+		await tick() // wait until the DOM is up to date
 
-		while (i < items.length) {
-			const row_height = height_map[i] || average_height;
-			if (y + row_height > scrollTop) {
-				start = i;
-				top = y;
+		let contentHeight = top - (viewport?.scrollTop ?? 0)
+		let i = start
 
-				break;
+		while (contentHeight < viewportHeight && i < items.length) {
+			let row = rows[i - start]
+
+			if (!row) {
+				end = i + 1
+				await tick() // render the newly visible row
+				row = rows[i - start]
 			}
 
-			y += row_height;
-			i += 1;
+			const rowHeight = (heightMap[i] = itemHeight || row.offsetHeight)
+			contentHeight += rowHeight
+			i += 1
+		}
+
+		end = i
+
+		const remaining = items.length - end
+		averageHeight = (top + contentHeight) / end
+
+		bottom = remaining * averageHeight
+		heightMap.length = items.length
+	}
+
+	async function handleScroll() {
+		const scrollTop = viewport?.scrollTop ?? 0
+
+		const oldStart = start
+
+		for (let v = 0; v < rows.length; v += 1) {
+			heightMap[start + v] = itemHeight || rows[v].offsetHeight
+		}
+
+		let i = 0
+		let y = 0
+
+		while (i < items.length) {
+			const rowHeight = heightMap[i] || averageHeight
+			if (y + rowHeight > scrollTop) {
+				start = i
+				top = y
+
+				break
+			}
+
+			y += rowHeight
+			i += 1
 		}
 
 		while (i < items.length) {
-			y += height_map[i] || average_height;
-			i += 1;
+			y += heightMap[i] || averageHeight
+			i += 1
 
-			if (y > scrollTop + viewport_height) break;
+			if (y > scrollTop + viewportHeight) break
 		}
 
-		end = i;
+		end = i
 
-		const remaining = items.length - end;
-		average_height = y / end;
+		const remaining = items.length - end
+		averageHeight = y / end
 
-		while (i < items.length) height_map[i++] = average_height;
-		bottom = remaining * average_height;
+		while (i < items.length) heightMap[i++] = averageHeight
+		bottom = remaining * averageHeight
 
 		// TODO if we overestimated the space these
 		// rows would occupy we may need to add some
-		// more. maybe we can just call handle_scroll again?
+		// more. maybe we can just call handleScroll again?
 	}
 
 	export async function scrollToIndex(index: number, opts: ScrollToOptions) {
-		const { scrollTop } = viewport;
-		const itemsDelta = index - start;
-		const _itemHeight = itemHeight || average_height;
-		const distance = itemsDelta * _itemHeight;
-		opts = {
+		const itemsDelta = index - start
+		const _itemHeight = itemHeight || averageHeight
+		const distance = itemsDelta * _itemHeight
+
+		viewport?.scrollTo({
 			left: 0,
-			top: scrollTop + distance,
+			top: (viewport?.scrollTop ?? 0) + distance,
 			behavior: 'smooth',
-			...opts
-		};
-		viewport.scrollTo(opts);
+			...opts,
+		})
 	}
+
+	// whenever `items` changes, invalidate the current heightMap
+	$effect(() => {
+		if (mounted) {
+			refresh(items, viewportHeight, itemHeight)
+		}
+	})
 
 	// trigger initial refresh
 	onMount(() => {
-		rows = contents.getElementsByTagName(
-			'svelte-virtual-list-row'
-		) as HTMLCollectionOf<HTMLElement>;
-		mounted = true;
-	});
+		rows = contents?.getElementsByTagName('svelte-virtual-list-row') as HTMLCollectionOf<HTMLElement>
+		mounted = true
+	})
 </script>
 
 <svelte-virtual-list-viewport
 	bind:this={viewport}
-	bind:offsetHeight={viewport_height}
-	on:scroll={handle_scroll}
+	bind:offsetHeight={viewportHeight}
+	onscroll={handleScroll}
 	style="height: {height};"
 	tabindex="-1"
 >
@@ -147,7 +160,7 @@
 	>
 		{#each visible as row (row.index)}
 			<svelte-virtual-list-row>
-				<slot item={row.data}>Missing template</slot>
+				{@render children({ item: row.data })}
 			</svelte-virtual-list-row>
 		{/each}
 	</svelte-virtual-list-contents>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,23 +8,29 @@
 	<h1>svelte-virtual-list-ts</h1>
 	<h2>Test with 5 Items</h2>
 	<div style="height: 200px;">
-		<VirtualList items={generateThings(5)} let:item>
-			<!-- this will be rendered for each currently visible item -->
-			<p>{item.number}: {item.name}</p>
-		</VirtualList>
+		<VirtualList items={generateThings(5)} >
+			{#snippet children({ item })}
+						<!-- this will be rendered for each currently visible item -->
+				<p>{item.number}: {item.name}</p>
+								{/snippet}
+				</VirtualList>
 	</div>
 	<h2>Test with 100 Items</h2>
 	<div style="height: 200px;">
-		<VirtualList items={generateThings(100)} let:item>
-			<!-- this will be rendered for each currently visible item -->
-			<p>{item.number}: {item.name}</p>
-		</VirtualList>
+		<VirtualList items={generateThings(100)} >
+			{#snippet children({ item })}
+						<!-- this will be rendered for each currently visible item -->
+				<p>{item.number}: {item.name}</p>
+								{/snippet}
+				</VirtualList>
 	</div>
 	<h2>Test with 10.000 Items</h2>
 	<div style="height: 200px;">
-		<VirtualList items={generateThings(10000)} let:item>
-			<!-- this will be rendered for each currently visible item -->
-			<p>{item.number}: {item.name}</p>
-		</VirtualList>
+		<VirtualList items={generateThings(10000)} >
+			{#snippet children({ item })}
+						<!-- this will be rendered for each currently visible item -->
+				<p>{item.number}: {item.name}</p>
+								{/snippet}
+				</VirtualList>
 	</div>
 </div>
